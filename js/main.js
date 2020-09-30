8355e560de1e90c1bf8a869e2b096ef3abb42f63
hideto hujiwara <ripton.0503@gmail.com>
2020-09-30 12:25:15 +0900

    ウィンドウサイズ変更の時際のCSSを追加

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
 $(function () {
     const $win = $(window);
-    const $btnMenu = $('.btn-menu');
-    const $nav = $('header > nav');
-    const isAnimate = 'is-animate';
-    const isOpen = 'is-open';
+    //スマホメニュー
+    (function () {
+        const $btnMenu = $('.btn-menu'); //クラスを関数に格納
+        const $nav = $('header > nav'); 
+        const isAnimate = 'is-animate'; //新しい機能を持たせたクラスを関数に格納
+        const isOpen = 'is-open'; 
 
-    $btnMenu.on('click', function () {
-
-        //ボタンをクリックした時の記述
-        if (!$(this).hasClass(isOpen)) { //$(this)は＄btnMenuのこと　＄btnMenuにisOpenがなかったら
-            $nav.slideDown(); //メニューを表示
-            $(this).addClass(isOpen).addClass(isAnimate); //isOpenを追加
-        } else { //btnMenuがis-openを持っていいたら
-            $nav.slideUp(); //メニューをスライドダウンで非表示
-            $btnMenu.removeClass(isOpen).removeClass(isAnimate); //is-openクラスを削除
-        }
-    });
-    // ウィンドウリサイズの処理
-    $win.on('resize', function () {
-        if ($win.width() > 768) {
-    
-            $nav.show();
-            $btnMenu.removeClass(isOpen).removeClass(isAnimate);
-        } else {
-            if (!$btnMenu.hasClass(isOpen)) {
-                $nav.hide();
+        // ボタンをクリック時の処理　
+        $btnMenu.on('click', function () { //btmMenuがクリックされた時
+            const $this = $(this); //イベントを格納
+            if ($nav.is(':animated')) { //結果をanimatedに返す
+                return;
             }
-        }
+            if (!$this.hasClass(isOpen)) { //isOpenを持ってない場合
+                $nav.slideDown(); //slideDownを付与
+                $this.addClass(isOpen).addClass(isAnimate); //クラスを付与
+            } else { //isOpenクラスを持っている場合
+                $nav.slideUp(); //slideUpを付与
+                $this.removeClass(isOpen).removeClass(isAnimate);　//クラスの削除
+            }
+        });
+
+    })();
+// ウィンドウリサイズ時の処理
+$win.on('resize', function () {
+    if ($win.width() > 768) { //ウィンドウサイズが768以下の場合
+      $nav.show(); //navの非表示状態の要素を表示
+      $btnMenu.removeClass(isOpen).removeClass(isAnimate); //クラスの削除
+    } else {
+      if (!$btnMenu.hasClass(isOpen)) {
+        $nav.hide();
+      }
+    }
+  });
+
+
+// 見出しのアニメーション
+(function () {
+  $win.on('scroll', function () {
+    const $heading = $('.mask_inner');
+    const isAnimate = 'is-animate';
+    $heading.each(function () {
+      const $this = $(this);
+      const headingTop = $this.offset().top;
+      const scrollPos = $win.scrollTop();
+      const wh = $win.height();
+      if (scrollPos > headingTop - wh + (wh / 4)) {
+        $this.addClass(isAnimate);
+      }
     });
+  });
+    
+})();
 });
